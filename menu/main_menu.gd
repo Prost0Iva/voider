31d31b2a1b611746b36342ec3ead0b53a2e0e04e
Prost0Iva <ivaprostoiva@gmail.com>
2026-03-31 21:52:39 +0300

    start main_menu and saves

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -1,7 +1,23 @@
 extends Node2D
 
+@onready var background_layers = [
+	$Background/Layer,
+	$Background/Layer2,
+	$Background/Layer3,
+	$Background/Layer4
+]
+
 func _ready() -> void:
 	Gui.init_buttons(self)
 
 func _process(_delta: float) -> void:
-	pass
+	parallax()
+
+func parallax():
+	var strengths = [5, 30, 20, 40]
+	var mouse_pos = get_viewport().get_mouse_position()
+	var screen_size = get_viewport().get_visible_rect().size
+	var offset = (mouse_pos - screen_size / 2) / (screen_size / 2)
+	for i in background_layers.size():
+		var layer = background_layers[i]
+		layer.position = offset * strengths[i]
