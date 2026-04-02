686ad0c1dff2d77e06cbc52dd366efad133a9049
Prost0Iva <ivaprostoiva@gmail.com>
2026-04-02 10:10:51 +0300

    fix button

--- classes/gui.gd
+++ classes/gui.gd
@@ -4,10 +4,15 @@ static func init_buttons(tree: Node):
 	var buttons = tree.get_tree().get_nodes_in_group("Button")
 	for button in buttons:
 		button.mouse_entered.connect(_on_button_mouse_entered.bind(button))
-	for button in buttons:
 		button.mouse_exited.connect(_on_button_mouse_exited.bind(button))
+		button.input_event.connect(_on_button_input_event)
 
 static func _on_button_mouse_entered(button: Node):
-	button.icon.region.position.x = 128
+	var texture = button.get_child(0)
+	texture.texture.region.position.x = 128
 static func _on_button_mouse_exited(button: Node):
-	button.icon.region.position.x = 0
+	var texture = button.get_child(0)
+	texture.texture.region.position.x = 0
+static func _on_button_input_event(viewport: Node, event: InputEvent, shape_idx: int):
+	if event.is_action_pressed("LeftClick"):
+		print("Watafa")
--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -1,20 +1,21 @@
 extends Node2D
 
 @onready var background_layers = [
-	$Background/Layer,
-	$Background/Layer2,
-	$Background/Layer3,
-	$Background/Layer4
+	$CanvasLayer/Background/Layer,
+	$CanvasLayer/Background/Layer2,
+	$CanvasLayer/Background/Layer3,
+	$CanvasLayer/Background/Layer4
 ]
 
 func _ready() -> void:
 	Gui.init_buttons(self)
+	pass
 
 func _process(_delta: float) -> void:
 	parallax()
 
 func parallax():
-	var strengths = [5, 30, 20, 40]
+	var strengths = [5, 20, 15, 35]
 	var mouse_pos = get_viewport().get_mouse_position()
 	var screen_size = get_viewport().get_visible_rect().size
 	var offset = (mouse_pos - screen_size / 2) / (screen_size / 2)
